647531270669395c56acde3c3644648c91095531
Dario Zela <dariozela1@gmail.com>
2023-05-09 17:17:30 +0100

    Extra Files

---
+++ Blaze/Core/ButtonExtentions.cs
@@ -0,0 +1,23 @@
+﻿using MahApps.Metro.IconPacks;
+using System.Windows;
+using System.Windows.Media;
+using Blaze.Core.Extentions;
+
+namespace Blaze.Core
+{
+    public class Icon : IExtention<PackIconCodiconsKind, Icon> 
+    { 
+    }
+
+    public class CornerRadiusExtention : IExtention<CornerRadius, CornerRadiusExtention>
+    {
+    }
+
+    public class IsMouseOverColor : IExtention<Brush, IsMouseOverColor>
+    {
+    }
+
+    public class IsPressedColor : IExtention<Brush, IsPressedColor>
+    {
+    }
+}
---
+++ Blaze/Core/ExtentionProperty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MahApps.Metro.IconPacks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Blaze.Core.Extentions
+{
+    public class IExtention<T, I>
+    {
+        public static DependencyProperty ExtendedProperty =
+            DependencyProperty.RegisterAttached("Property",
+                                                typeof(T),
+                                                typeof(IExtention<T, I>),
+                                                new PropertyMetadata(default(T)));
+
+        public static T GetProperty(DependencyObject target)
+        {
+            return (T)target.GetValue(ExtendedProperty);
+        }
+
+        public static void SetProperty(DependencyObject target, T value)
+        {
+            target.SetValue(ExtendedProperty, value);
+        }
+    }
+}
